1d32f8bc1104a6c0c0295ba068cb9701700c68e1
Carrott99 <pedro.carrott.99@gmail.com>
2020-12-10 18:44:42 +0000

    Send cuckoo filter via push notifications

--- src/main/java/com/cuckoo/BackendServer/service/ContactTracingService.java
+++ src/main/java/com/cuckoo/BackendServer/service/ContactTracingService.java
@@ -3,12 +3,19 @@ package com.cuckoo.BackendServer.service;
 import com.cuckoo.BackendServer.models.contactTracing.CuckooFilter;
 import com.cuckoo.BackendServer.models.contactTracing.patient.Patient;
 import com.cuckoo.BackendServer.models.contactTracing.patient.PatientDto;
+import com.cuckoo.BackendServer.service.remoteServices.NotificationsRemoteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
 import java.util.Set;
 
 @Service
 public class ContactTracingService {
+
+    @Autowired
+    NotificationsRemoteService notificationsRemoteService;
+
     public void addDoctorInformation() {
         //DAVID: BAH
     }
@@ -17,6 +24,6 @@ public class ContactTracingService {
         // TODO Match medicHash and infectedEpoch with stored medic info
         CuckooFilter filter = new CuckooFilter();
         patientData.stream().map(Patient::new).map(Patient::patientHash).forEach(filter::insert);
-        // TODO Send cuckoo filter via push notifications
+        notificationsRemoteService.sendDataNotification(Map.of("filter", filter.toString()));
     }
 }
